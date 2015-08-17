5334a334b8327566230741cba5971391f87b164c
Chris Smith <cleesmith2006@gmail.com>
2015-08-17 07:46:35 -0400

    update

--- xml/main.go
+++ xml/main.go
@@ -19,8 +19,11 @@ import (
 // 'views',       "//div[@class='watch-view-count']/text()"
 // 'thumbsup',    "//button[@id='watch-like']/span/text()"
 // 'thumbsdown',  "//button[@id='watch-dislike']/span/text()"
-// 'subscribers', r"subscriber-count.*?>(?P<scrape>[0-9,]+?)<"
-// 'subscribers', "//*[@id="browse-items-primary"]/li/div/div[1]/div/span[1]/b"
+// via any youtube video page:
+// 'subscribers', "//*[@id="watch7-subscription-container"]/span/span[@class='yt-subscription-button-subscriber-count-branded-horizontal ']/text()"
+// via youtube user's about page:
+// 'subscribers', "//*[@id='browse-items-primary']/li/div/div/div/span[@class='about-stat']/b/text()"
+// 'subscribers', r"subscriber-count.*?>(?P<scrape>[0-9,]+?)<" ... old regex way
 // 'ga',          r"(?:\'|\")(?P<scrape>UA-.*?)(?:\'|\")"
 
 func main() {
@@ -28,7 +31,8 @@ func main() {
 	// resp, err := http.Get("http://cellipede.com:4235/")
 	// resp, err := http.Get("http://cleesmith.github.io/")
 	// resp, err := http.Get("https://github.com/cleesmith")
-	resp, err := http.Get("https://www.youtube.com/user/cleesmith2006/about")
+	// resp, err := http.Get("https://www.youtube.com/user/cleesmith2006/about")
+	resp, err := http.Get("https://www.youtube.com/watch?v=Eacoqt4BtMc")
 	if err != nil {
 		fmt.Printf("ERROR: http.Get: %v\n", err)
 		panic(err)
@@ -55,8 +59,13 @@ func main() {
 	// xp := xpath.Compile("/html/body/hr")
 	// xp := xpath.Compile("//title/text()")
 	// xp := xpath.Compile("//meta[translate(@name, 'ABCDEFGHJIKLMNOPQRSTUVWXYZ', 'abcdefghjiklmnopqrstuvwxyz')='description']/@content")
-	// xp := xpath.Compile("//*[@id='browse-items-primary']/li/div/div[1]/div/span[1]/b")
-	xp := xpath.Compile("//*[@id='browse-items-primary']/li/div/div/div/span[@class='about-stat']/b/text()")
+
+	// youtube user's about page, get both subscribers and total views:
+	// xp := xpath.Compile("//*[@id='browse-items-primary']/li/div/div/div/span[@class='about-stat']/b/text()")
+
+	// get subscribers via any youtube video page:
+	xp := xpath.Compile("//*[@id='watch7-subscription-container']/span/span[@class='yt-subscription-button-subscriber-count-branded-horizontal ']/text()")
+
 	nodes, err := doc.Root().Search(xp)
 	if err != nil {
 		fmt.Printf("ERROR: doc.Root().Search(xp): %v\n", err)
@@ -65,9 +74,10 @@ func main() {
 	}
 	fmt.Printf("nodes=%T=%v=%v\n", nodes, len(nodes), nodes)
 	if len(nodes) > 0 {
-		attrValue := nodes[0].String()
-		fmt.Printf("nodes[0]=%T=%v \nattrValue=%T=%s\n",
-			nodes[0], nodes[0].String(), attrValue, attrValue)
+		subscribers := nodes[0].String()
+		fmt.Printf("nodes[0]=%T=%v \nsubscribers=%T=%s\n", nodes[0], nodes[0].String(), subscribers, subscribers)
+		// totalViews := nodes[1].String()
+		// fmt.Printf("nodes[1]=%T=%v \ntotalViews=%T=%s\n", nodes[1], nodes[1].String(), totalViews, totalViews)
 		fmt.Println("\n- nodes matching search:")
 		for n := range nodes {
 			fmt.Printf("\tnodes[%v]=%T=%s nodes[%v].Name()=%v\n",
